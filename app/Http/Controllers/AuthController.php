8df86cdc26618129625a2884c85cb28a6d7dba50
briant24 <briantadr24@gmail.com>
2020-08-09 22:46:16 +0700

    endpoint

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function me()
+    {
+        return "NIS     :   3103118035
+                Nama    :   Briant Audiera
+                Gender  :   Laki-Laki
+                Phone   :   081238627989
+                Kelas   :   XII RPL 1";
+    }
+}
